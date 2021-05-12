5d13b04b7d1923e80f073352706db6ba5f700c59
Leon Stafford <me@ljs.dev>
2021-05-12 22:21:23 +1000

    wip comments

--- test/myasynctest.js
+++ test/myasynctest.js
@@ -2,24 +2,34 @@ const chai = require('chai');
 
 const { expect } = chai;
 
+// TODO: asynchronously fetch data from here for each plugin
 // get list of plugin downloads by day at:
 // http://api.wordpress.org/stats/plugin/1.0/downloads.php?slug=wordpress-seo
 
+// static list of plugins we want to get some data about from API
 const somePlugins = [
   'wordpress-seo',
   'statically',
   'autoptimize',
 ];
 
+// dummmy promise to test we know how to chain things up
 const myPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve('foo');
   }, 300);
 });
 
+// out Mocha tests
 describe('Test asynchronous code', () => {
-  it('should return another output', () => callToAsyncFunction(input).then((result) => {
-    // implement result testing logic
+  it('should return another output', async () =>{
+
+    // do a bunch of stuff, leading to the value/condition we want to assert on
+    myPromise().then((result) => {
+      // implement result testing logic
+    });
+
+    // our Chai assertion
     expect(foo).to.be.a('string');
-  }));
+  });
 });
