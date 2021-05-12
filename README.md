51ae1fca54716a0392bd44d865d70f8e5f8cbba4
Leon Stafford <me@ljs.dev>
2021-05-12 22:03:36 +1000

    some linting/basic instructions

---
+++ README.md
@@ -0,0 +1,11 @@
+# Async demo
+
+show simple example of mocha/chai to test some async functions
+
+### run tests
+
+ - `./node_modules/.bin/mocha`
+
+### lint code
+
+ - `./node_modules/.bin/eslint test/myasynctest.js`
--- test/myasynctest.js
+++ test/myasynctest.js
@@ -1,11 +1,25 @@
-const chai = require("chai")
-const expect = chai.expect
+const chai = require('chai');
 
-describe("Test asynchronous code", () => {
-  it("should return another output", () => {
-      return callToAsyncFunction(input).then(result => {
-          //implement result testing logic
-          expect(foo).to.be.a('string');
-      })
-  })
+const { expect } = chai;
+
+// get list of plugin downloads by day at:
+// http://api.wordpress.org/stats/plugin/1.0/downloads.php?slug=wordpress-seo
+
+const somePlugins = [
+  'wordpress-seo',
+  'statically',
+  'autoptimize',
+];
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('foo');
+  }, 300);
+});
+
+describe('Test asynchronous code', () => {
+  it('should return another output', () => callToAsyncFunction(input).then((result) => {
+    // implement result testing logic
+    expect(foo).to.be.a('string');
+  }));
 });
